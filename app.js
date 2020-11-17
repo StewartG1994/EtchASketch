ea1fa86f1c1283302f468a0d604ed96acaabccb8
Stewart Gliniecki <stewgliniecki@gmail.com>
2020-11-17 11:41:58 +0000

    background colour change function

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const container = document.getElementById("container");
+const grid = document.querySelectorAll('grid');
+const div = document.querySelectorAll('div')
+console.log(container, div, grid)
 
-function makeRows(rows, cols) {
+
+function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     [c];
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "grid";
   };
 };
 
-makeRows(16, 16);
+function sizeSelect (){
+    const number = prompt("Please enter a number between 10 and 64");
+    if (number >= 10 && number <=64)
+        makeGrid(number,number);
+    }
+
+sizeSelect()
+
+
+  container.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'red';
+  })
