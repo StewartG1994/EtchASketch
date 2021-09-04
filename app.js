72a1b31935c8c4177fefd6eba45082fb941f06b1
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-04 22:25:53 +0100

    random color & red color hover

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
-
 const container = document.querySelector('.container');
-console.log(container)
+const buttons =document.querySelector('.buttons');
+const redBtn = document.getElementById('red');
+const random = document.getElementById('random');
+const reset = document.getElementById('reset');
 
+function randomColor() {
+  let color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return 'rgb(' + color.join(', ') + ')';
+} 
+
+function reloadThePage(){
+  window.location.reload();
+} 
 
 function createDiv(number) {
 
 container.style.setProperty('--rows', number);
 
-
 for (i = 0; i < (number* number); i++){
   const div = document.createElement('div')
   container.appendChild(div).className = 'div';
   [i];
+}}
 
-}
+let gridSize = prompt('Enter a number between 16 and 64')
+createDiv(gridSize)
 
-}
 
-createDiv(40)
-
-container.addEventListener('mouseover', event => {
+redBtn.addEventListener('click', () => {
+  container.addEventListener('mouseover', event => {
   event.target.style.backgroundColor = 'red';
+  })
+});
+
+random.addEventListener('click', ()=>{
+
+  container.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = randomColor();
+  })
 })
 
+reset.addEventListener('click', reloadThePage)
 
 
 
 
 
-// create div * number required 
